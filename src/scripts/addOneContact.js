53779f5060236d493e0c88a001dea91744f3e27b
Daria H <dholubenna@gamil.com>
2025-09-05 22:20:57 +0300

    fix func addOne + UPD generateContacts

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,7 +1,7 @@
 import { createFakeContact } from '../utils/createFakeContact.js';
 import { writeContacts } from '../utils/writeContacts.js';
-import { PATH_DB } from '../constants/contacts.js';
-import fs from 'node:fs/promises';
+// import { PATH_DB } from '../constants/contacts.js';
+// import fs from 'node:fs/promises';
 import { log } from 'node:console';
 import { readContacts } from '../utils/readContacts.js';
 
@@ -10,12 +10,12 @@ export const addOneContact = async () => {
   const createContact = createFakeContact();
   data.push(createContact);
   const dataString = JSON.stringify(data, null, 2);
-  await fs.appendFile(PATH_DB, dataString, 'utf8');
-  try {
-    log('success appendFile');
-  } catch (error) {
-    log(`error appendFile`, error);
-  }
+  // await fs.appendFile(PATH_DB, dataString, 'utf8');
+  // try {
+  //   log('success appendFile');
+  // } catch (error) {
+  //   log(`error appendFile`, error);
+  // }
   writeContacts(dataString);
   log(data);
 };
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -7,20 +7,21 @@ import { readContacts } from '../utils/readContacts.js';
 
 const generateContacts = async (number) => {
   const data = await readContacts(); 
-    log(data);
+    log(typeof data);
   for (let i = 0; i < number; i++) {
     const createContact = createFakeContact(); 
          data.push(createContact);
+             log(typeof data);
   }
       const dataString = JSON.stringify(data, null, 2);
-  await fs.appendFile(PATH_DB, dataString, 'utf8');
-      try {
-      log('success appendFile');
-    } catch (error) {
-      log(`error appendFile`, error);
-    }
-    log(data);
+  // await fs.appendFile(PATH_DB, dataString, 'utf8');
+  //     try {
+  //     log('success appendFile');
+  //   } catch (error) {
+  //     log(`error appendFile`, error);
+  //   }
+    // log(data);
 writeContacts(dataString);
 };
 
-generateContacts(1);
+generateContacts(3);
